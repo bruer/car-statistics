d03924edaf683f9d45f0fe2a94acb31d67729986
Jens Bruer <jbruer@gmail.com>
2021-07-16 15:43:22 +0200

    added test_js db

--- src/utils/sqlite3db.js
+++ src/utils/sqlite3db.js
@@ -1,22 +1,29 @@
 const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('/Users/jbruer/Development/sqlite/db/car-statistics.db', (err) => {
+let test_data = {}
+// let db = new sqlite3.Database('/Users/jbruer/Development/sqlite/db/car-statistics.db', (err) => {
+//     if (err) {
+//       console.error(err.message);
+//     }
+//     console.log('Connected to the car-statistics database.');
+//   });
+  let db_test = new sqlite3.Database('/Users/jbruer/Development/sqlite/db/test_js.db', (err) => {
     if (err) {
       console.error(err.message);
     }
-    console.log('Connected to the car-statistics database.');
+    console.log('Connected to the test_js database.');
   });
 
-  db.serialize(() => {
-    db.each(`SELECT ID,
-                    Name as name
-             FROM used_car_prices2`, (err, row) => {
-      if (err) {
-        console.error(err.message);
-      }
-      console.log(row.id + "\t" + row.name);
-    });
-  });
-
+//   db.serialize(() => {
+//     db.each(`SELECT ID,
+//                     Name as name
+//              FROM used_car_prices2`, (err, row) => {
+//       if (err) {
+//         console.error(err.message);
+//       }
+//       console.log(row.id + "\t" + row.name);
+//     });
+//   });
+  let sql_statement_test = `SELECT * FROM people`;
   let sql = `SELECT 
                   DISTINCT DATE date,
                   MODEL model,
@@ -27,20 +34,45 @@ let db = new sqlite3.Database('/Users/jbruer/Development/sqlite/db/car-statistic
             GROUP BY DATE
             `;
 
-db.each(sql, ["2021-01-01"], (err, row) => {
+// db.each(sql, ["2021-01-01"], (err, row) => {
+//     //console.log(`${row.id} ---${row.date} -- ${row.model} -- ${row.price}`);
+//   if (err) {
+//     throw err;
+//   }
+//   let model3_data = {};
+//   model3_data[row.date] = row.price;
+//   //console.log(`${row.id} ---${row.date} -- ${row.model} -- ${row.price}`);
+//   for (const [key, value] of Object.entries(model3_data)) {
+//     console.log(key, value);
+//   }
+// });
+
+// db.each(sql, ["2021-01-01"], (err, row) => {
+//     //console.log(`${row.id} ---${row.date} -- ${row.model} -- ${row.price}`);
+//   if (err) {
+//     throw err;
+//   }
+//   let data = {};
+//   data[row.name] = row.age;
+//   //console.log(`${row.id} ---${row.date} -- ${row.model} -- ${row.price}`);
+//   for (const [key, value] of Object.entries(data)) {
+//     console.log(key, value);
+//   }
+// });
+
+db_test.each(sql_statement_test, (err, row) => {
     //console.log(`${row.id} ---${row.date} -- ${row.model} -- ${row.price}`);
   if (err) {
     throw err;
   }
-  let model3_data = {};
-  model3_data[row.date] = row.price;
+  test_data[row.name] = row.age;
   //console.log(`${row.id} ---${row.date} -- ${row.model} -- ${row.price}`);
-  for (const [key, value] of Object.entries(model3_data)) {
+  for (const [key, value] of Object.entries(test_data)) {
     console.log(key, value);
   }
 });
   
-  db.close((err) => {
+db_test.close((err) => {
     if (err) {
       console.error(err.message);
     }
