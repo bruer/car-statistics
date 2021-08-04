04179b13b04d3b1a42c33e75a64a94b026c60c27
bruer <f.bruer@gmail.com>
2021-08-04 12:46:51 +0200

    testing around

--- src/utils/sqlite3db.js
+++ src/utils/sqlite3db.js
@@ -1,23 +1,26 @@
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('/Users/jbruer/Development/sqlite/db/car-statistics.db', (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Connected to the car-statistics database.');
-  });
+const sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database("../db/car-statistics.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to the car-statistics database.");
+});
 
-  db.serialize(() => {
-    db.each(`SELECT ID,
+db.serialize(() => {
+  db.each(
+    `SELECT ID as id,
                     Name as name
-             FROM used_car_prices2`, (err, row) => {
+             FROM used_car_prices`,
+    (err, row) => {
       if (err) {
         console.error(err.message);
       }
       console.log(row.id + "\t" + row.name);
-    });
-  });
+    }
+  );
+});
 
-  let sql = `SELECT 
+let sql = `SELECT 
                   DISTINCT DATE date,
                   MODEL model,
                   PRICE price
@@ -27,22 +30,9 @@ let db = new sqlite3.Database('/Users/jbruer/Development/sqlite/db/car-statistic
             GROUP BY DATE
             `;
 
-db.each(sql, ["2021-01-01"], (err, row) => {
-    //console.log(`${row.id} ---${row.date} -- ${row.model} -- ${row.price}`);
+db.close((err) => {
   if (err) {
-    throw err;
+    console.error(err.message);
   }
-  let model3_data = {};
-  model3_data[row.date] = row.price;
-  //console.log(`${row.id} ---${row.date} -- ${row.model} -- ${row.price}`);
-  for (const [key, value] of Object.entries(model3_data)) {
-    console.log(key, value);
-  }
+  console.log("Close the database connection.");
 });
-  
-  db.close((err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Close the database connection.');
-  });
